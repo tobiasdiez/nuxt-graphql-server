e3de70b7f92d9eac98ef7cd9cbe974475c0a3a6e
Tobias Diez <code@tobiasdiez.com>
2022-10-26 22:33:53 +0000

    feat: add typescript info for virtual schema import

---
+++ src/graphql-server.d.ts
@@ -0,0 +1,3 @@
+import { GraphQLSchema } from 'graphql'
+
+export const schema: GraphQLSchema
--- src/module.ts
+++ src/module.ts
@@ -1,4 +1,5 @@
-import { defineNuxtModule } from '@nuxt/kit'
+import { addTemplate, createResolver, defineNuxtModule } from '@nuxt/kit'
+import { relative } from 'path'
 import { createSchemaImport } from './schema-loader'
 
 export interface ModuleOptions {
@@ -14,13 +15,30 @@ export default defineNuxtModule<ModuleOptions>({
     schema: './server/**/*.graphql',
   },
   setup(options, nuxt) {
+    // eslint-disable-next-line @typescript-eslint/unbound-method
+    const { resolve } = createResolver(import.meta.url)
+
     nuxt.hook('nitro:config', async (nitroConfig) => {
       // Register #graphql/schema virtual module
       nitroConfig.virtual = nitroConfig.virtual || {}
-      nitroConfig.virtual['#graphql/schema'] = await createSchemaImport(
+      nitroConfig.virtual['#' + 'graphql/schema'] = await createSchemaImport(
         options.schema,
         nitroConfig.rootDir
       )
+    })
+
+    // Create types in build dir
+    const { dst: typeDefPath } = addTemplate({
+      filename: 'graphql-server.d.ts',
+      src: resolve('graphql-server.d.ts'),
+    })
+
+    // Add types to `nuxt.d.ts`
+    nuxt.hook('prepare:types', ({ references, tsConfig }) => {
+      tsConfig.compilerOptions.paths['#' + 'graphql/schema'] = [
+        relative(nuxt.options.rootDir, typeDefPath),
+      ]
+      references.push({ path: 'graphql-server.d.ts' })
     })
   },
 })
