97fefb7a4ce1884ba00028b8450c0d7e4842ce73
Tobias Diez <code@tobiasdiez.com>
2022-10-26 22:35:11 +0000

    chore: fix eslint

--- playground/server/api/GetSchema.ts
+++ playground/server/api/GetSchema.ts
@@ -3,4 +3,5 @@ import { schema } from '#graphql/schema'
 // API endpoint that exposes the schema as JSON.
 // In production, use introspection instead!
 // This is here as demonstration and to easily test HMR.
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call
 export default defineEventHandler(() => schema)
