0b0761eb4e69ec9f87d4a8d84e1bbbe4fa0fd8da
Tobias Diez <code@tobiasdiez.de>
2025-04-10 19:11:40 +0800

    fix linter

--- build.config.ts
+++ build.config.ts
@@ -3,10 +3,10 @@ import { defineBuildConfig } from 'unbuild'
 export default defineBuildConfig({
   entries: [
     {
-      builder: "copy",
+      builder: 'copy',
       input: 'src',
       pattern: 'graphql-server.d.ts',
-      outDir: 'dist'
+      outDir: 'dist',
     },
-  ]
+  ],
 })
